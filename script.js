9f2501df67ebad4a94310ef9d107166e002d5b6a
Dinkar B Panchal <dinkarpanchal7758@gmail.com>
2022-09-24 21:10:12 +0530

    Resume

---
+++ script.js
@@ -0,0 +1,20 @@
+
+function ChangeBG()
+{
+    // document.getElementById("head").innerHTML="Paragraph Changed ";
+    document.getElementById("head").style.background="rgb(6, 153, 244)";
+    document.getElementById("head").style.color="white";
+
+}
+function ChangeBG1()
+{
+    document.getElementById("head").style.background="white";
+    document.getElementById("head").style.color="blue";
+
+}
+function ChangeBG2()
+{
+    document.getElementById("career").style.background="white";
+    document.getElementById("career").style.color="blue";
+
+}
